5e869a6004fa752165441d9aae6b0733884b4627
Alex <foxwalkin@gmail.com>
2018-12-28 03:07:01 -0500

    version change

--- source/irc.c
+++ source/irc.c
@@ -39,19 +39,21 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC5-2.0.1";
-const char useful_info[] = "epic5 2 0 1";
+const char irc_version[] = "EPIC5twitch-0.1.0";
+const char useful_info[] = "epic5twitch 0 1 0";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20160805";
+const char internal_version[] = "20181228";
 
 /*
  * In theory, this number is incremented for every commit.
+ * Note: This was too high by 7 even after all my commits
+ * to the Twitch version fork, which is odd.
  */
-const unsigned long	commit_id = 1897;
+const unsigned long	commit_id = 1890;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
